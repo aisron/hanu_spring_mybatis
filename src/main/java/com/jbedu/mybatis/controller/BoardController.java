65bebf69a8f73cd91ae4bcd1729ffa0c58c2d771
JBEDU16 <JBEDU16@DESKTOP-HMF1SKG>
2024-12-23 11:25:49 +0900

    글쓰기 폼, 글쓰기 넘김 , 글 작성한거 넘길 거 작성

--- src/main/java/com/jbedu/mybatis/controller/BoardController.java
+++ src/main/java/com/jbedu/mybatis/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.jbedu.mybatis.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +18,15 @@ public class BoardController {
 	@RequestMapping(value ="write_form")
 	public String write_form() {
 		return "write_form";
+	}
+	
+	@RequestMapping(value="/writeOK")
+	public String boardWrite(HttpServletRequest request, Model model) {	
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		return "redirect:boardList";//
 	}
 	
 }
